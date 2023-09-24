17028f6df3cb4cf8ac8cd81394be0a961b52fdbd
Qwerty1029 <skydiving123@naver.com>
2023-09-24 18:24:15 +0900

    Firebase로 알람 기능 구현, androidmanifast.xml관련 오류 해결
    1. 앱 실행중이 아니더라도(Pause일 떄, 앱이 종료 되었을 때 모두) 알림을 보내면 수신함
    2. AndroidManifast.xml관련 오류 완전히 해결
        -> Indent부분 수정이 필요함(kakaotalk authorization부분을 제거하였으므로)
    3. Firebase Configuration 구현 완료

--- lib/Component/Notification.dart
+++ lib/Component/Notification.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
 
 class FlutterLocalNotification {
   FlutterLocalNotification._();
@@ -52,5 +54,96 @@ class FlutterLocalNotification {
 
     await flutterLocalNotificationsPlugin.show(
         0, 'test title', 'test body', notificationDetails);
+  }
+}
+
+class NotificationController extends GetxController {
+  //메시징 서비스 기본 객체 생성
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  @override
+  void onInit() async {
+    debugPrint("NotificationController");
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    debugPrint("${settings.authorizationStatus}");
+    _getToken();
+    _onMessage();
+    super.onInit();
+  }
+
+  void _getToken() async {
+    String? token = await messaging.getToken();
+    try {
+      debugPrint(token);
+    } catch (e) {}
+  }
+
+  final AndroidNotificationChannel channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.max,
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  void _onMessage() async {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        android: AndroidInitializationSettings('mipmap/ic_launcher'),
+        iOS: DarwinInitializationSettings(
+          requestAlertPermission: false,
+          requestBadgePermission: false,
+          requestSoundPermission: false,
+        ),
+      ),
+    );
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              importance: Importance.max,
+              priority: Priority.max,
+            ),
+          ),
+        );
+      }
+
+      debugPrint('foreground 상황에서 메시지를 받았다.');
+
+      debugPrint('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        debugPrint(
+            'Message also contained a notification: ${message.notification!.body}');
+      }
+    });
   }
 }
